d376e005e61042cf583b25dcfa5c2b6b3c3612be
Mario Voigt <opensource@leyghis.de>
2020-07-23 02:20:09 +0200

    Update for InkScape 1.0

--- lasercut_sequence.py
+++ lasercut_sequence.py
@@ -2,48 +2,40 @@
 
 # We will use the inkex module with the predefined Effect base class.
 import inkex
-
 import measure
 import re
-import cubicsuperpath
-from simplepath import parsePath
+from inkex.paths import Path
+from inkex import paths
+
 # The simplestyle module provides functions for style parsing.
 from simplestyle import *
 
-
-
-
-
 def getArea(path):
-	return abs(measure.csparea(cubicsuperpath.parsePath(path + "z")))
+	return abs(measure.csparea(paths.CubicSuperPath(path + "z")))
 
 
 class LaserSort(inkex.Effect):
 	def __init__(self):
 		inkex.Effect.__init__(self)
-
-
-
-
-		
+	
 	def effect(self):
 		elements = self.document.xpath('//svg:path',namespaces=inkex.NSS)
 		for el in elements:
 
 			oldpathstring = el.attrib['d']
 
-			nodes = parsePath(oldpathstring)
+			nodes = Path(oldpathstring).to_arrays()
 
 			currentSection = []
 			sections = [currentSection]
 			for node in nodes:
 				command = node.pop(0)
-				currentSection.append(command + ' ' + ' '.join(map(lambda c: ','.join(map(str, c)), node)))
+				currentSection.append(command + ' ' + ' '.join(list(map(lambda c: ','.join(map(str, c)), node))))
 				if command.lower() == 'z':
 					currentSection = []
 					sections.append(currentSection)
 			
-			sections = map(lambda n: ' '.join(n), filter(lambda n: len(n) > 0, sections))
+			sections = list(map(lambda n: ' '.join(n), filter(lambda n: len(n) > 0, sections)))
 
 			if (sections[-1][-2].lower() != 'z'):
 				nonClosedSection = ' ' + sections.pop()
@@ -59,10 +51,6 @@ class LaserSort(inkex.Effect):
 			newpathstring = "z ".join(sections) + nonClosedSection
 			el.set('d', newpathstring)
 		
-
-
-
-
 # Create effect instance and apply it.
 effect = LaserSort()
-effect.affect()
+effect.run()
